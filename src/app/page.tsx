45d1252366452ab68847abd4456b763ceac6c8ff
Chukwuemeka Matthew <matthew.chukwuemeka40@gmail.com>
2023-10-11 01:37:43 +0100

    get user publicKey and keypair structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,15 @@ import { SlopeAdapter } from "@web3auth/slope-adapter";
 import { SolanaWalletConnectorPlugin } from "@web3auth/solana-wallet-connector-plugin";
 // Adapters
 import { SolflareAdapter } from "@web3auth/solflare-adapter";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 import RPC from "./solanaRPC";
 // import UploadToPinata from "@/components/UploadToPinata";
 
 // import { useDispatch } from "react-redux";
 // import { setUser, selectUser } from "@/store/slice/userSlice";
+const bs58 = require('bs58');
+import { Keypair } from "@solana/web3.js"
 
 const clientId =
   "BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk"; // get from https://dashboard.web3auth.io
@@ -33,95 +35,147 @@ export default function Home() {
 
   // const dispatch = useDispatch();
 
+  // useEffect(() => {
+  //   const init = async () => {
+  //     try {
+  //       const web3authInstance = new Web3Auth({
+  //         clientId,
+  //         chainConfig: {
+  //           chainNamespace: CHAIN_NAMESPACES.SOLANA,
+  //           chainId: "0x3", // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
+  //           rpcTarget: "https://api.devnet.solana.com", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+  //         },
+  //         // uiConfig refers to the whitelabeling options, which is available only on Growth Plan and above
+  //         // Please remove this parameter if you're on the Base Plan
+  //         uiConfig: {
+  //           appName: "W3A",
+  //           // appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
+  //           theme: {
+  //             primary: "red",
+  //           },
+  //           mode: "dark",
+  //           logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+  //           logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+  //           defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+  //           loginGridCol: 3,
+  //           primaryButton: "externalLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+  //         },
+  //         web3AuthNetwork: "cyan",
+  //       });
+
+  //       // adding solana wallet connector plugin
+
+  //       const torusPlugin = new SolanaWalletConnectorPlugin({
+  //         torusWalletOpts: {},
+  //         walletInitOptions: {
+  //           whiteLabel: {
+  //             name: "Whitelabel Demo",
+  //             theme: { isDark: true, colors: { torusBrand1: "#00a8ff" } },
+  //             logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+  //             logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+  //             topupHide: true,
+  //             defaultLanguage: "en",
+  //           },
+  //           enableLogging: true,
+  //         },
+  //       });
+  //       const value = await web3authInstance.addPlugin(torusPlugin);
+
+  //       const slopeAdapter = new SlopeAdapter({
+  //         clientId,
+  //       });
+  //       web3authInstance.configureAdapter(slopeAdapter);
+
+  //       setWeb3auth(web3authInstance);
+
+  //       await web3authInstance.initModal();
+  //       setProvider(web3authInstance.provider);
+
+  //       if (web3authInstance.connectedAdapterName) {
+  //         setLoggedIn(true);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   init();
+  // }, []);
+
+
   useEffect(() => {
     const init = async () => {
       try {
-        const web3authInstance = new Web3Auth({
-          clientId,
+        const web3auth = new Web3Auth({
+          clientId, 
+          web3AuthNetwork: "cyan",
           chainConfig: {
             chainNamespace: CHAIN_NAMESPACES.SOLANA,
-            chainId: "0x3", // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
-            rpcTarget: "https://api.devnet.solana.com", // This is the public RPC we have added, please pass on your own endpoint while creating an app
-          },
-          // uiConfig refers to the whitelabeling options, which is available only on Growth Plan and above
-          // Please remove this parameter if you're on the Base Plan
-          uiConfig: {
-            appName: "W3A",
-            // appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
-            theme: {
-              primary: "red",
-            },
-            mode: "dark",
-            logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-            logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
-            defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
-            loginGridCol: 3,
-            primaryButton: "externalLogin", // "externalLogin" | "socialLogin" | "emailLogin"
-          },
-          web3AuthNetwork: "cyan",
-        });
-
-        // adding solana wallet connector plugin
-
-        const torusPlugin = new SolanaWalletConnectorPlugin({
-          torusWalletOpts: {},
-          walletInitOptions: {
-            whiteLabel: {
-              name: "Whitelabel Demo",
-              theme: { isDark: true, colors: { torusBrand1: "#00a8ff" } },
-              logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-              logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
-              topupHide: true,
-              defaultLanguage: "en",
-            },
-            enableLogging: true,
+            chainId: "0x3",
+            rpcTarget: "https://api.devnet.solana.com",
           },
         });
-        await web3authInstance.addPlugin(torusPlugin);
 
-        const solflareAdapter = new SolflareAdapter({
-          clientId,
-        });
-        web3authInstance.configureAdapter(solflareAdapter);
+        setWeb3auth(web3auth)
+        await web3auth.initModal()
 
-        const slopeAdapter = new SlopeAdapter({
-          clientId,
-        });
-        web3authInstance.configureAdapter(slopeAdapter);
-
-        setWeb3auth(web3authInstance);
-
-        await web3authInstance.initModal();
-        setProvider(web3authInstance.provider);
-
-        if (web3authInstance.connectedAdapterName) {
-          setLoggedIn(true);
+        if (web3auth?.provider) {
+          setProvider(web3auth.provider)
+        } else {
+          console.log("no provider set")
         }
+  
       } catch (error) {
-        console.error(error);
+        console.log({error})
+        console.log(error)
       }
-    };
+    }
 
-    init();
-  }, []);
+    init()
 
-  // function uiConsole(...args: any[]): void {
-  //   const el = document.querySelector("#console>p");
-  //   if (el) {
-  //     el.innerHTML = JSON.stringify(args || {}, null, 2);
-  //   }
-  // }
+  }, [])
 
   const login = async () => {
-    if (!web3auth) {
-      // uiConsole("web3auth not initialized yet");
-      return;
-    }
-    const web3authProvider = await web3auth.connect();
-    setProvider(web3authProvider);
-    // console.log(web3authProvider);
-    // getUserInfo();
+      if (!web3auth) {
+        console.log("login: web3auth not initialized yet")
+        return
+      }
+      const web3authProvider = await web3auth.connect()
+      setProvider(provider)
+
+      const user = await web3auth.authenticateUser()
+      const idToken = user.idToken
+      console.log({user})
+      console.log({idToken})
+      await getPrivateKey()
   };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet")
+      return
+    }
+    const rpc = new RPC(provider)
+    const privateKey = await rpc.getPrivateKey()
+    uiConsole(privateKey)
+    console.log({privateKey})
+
+    const account = await rpc.getAccounts() // the first item on this list the public key we have been looking for
+    console.log({account})
+
+    const privateKeyBinary = Uint8Array.from(Buffer.from(privateKey, 'hex'))
+    const keypair = Keypair.fromSecretKey(privateKeyBinary)
+    const publicKey = keypair.publicKey
+    console.log({publicKey: publicKey.toString(), ba58: publicKey.toBase58()})
+    console.log({keypair})
+  }
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
 
   // const authenticateUser = async () => {
   //   console.log(web3auth);
