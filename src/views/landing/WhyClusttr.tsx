e0753652be04010eb1dc88aed9c2bdee59e02d53
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2024-05-21 10:07:35 +0100

    Completed the Co-ownership tab

--- src/views/landing/WhyClusttr.tsx
+++ src/views/landing/WhyClusttr.tsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 // import land from '../../assets/why_clusttr/land.png';
 // import coin from '../../assets/why_clusttr/coin.png';
 // import community from '../../assets/why_clusttr/community.png';
-import person1 from '../../assets/why_clusttr/person1.png';
-import person2 from '../../assets/why_clusttr/person2.png';
-import person3 from '../../assets/why_clusttr/person3.png';
-import person4 from '../../assets/why_clusttr/person4.png';
+import grid from '../../assets/why_clusttr/grid.png';
+
+import WhyClusttrCoOwnershipDesign from './WhyClusttrCoOwnershipDesign';
 
 const WhyClusttr = () => {
 	return (
@@ -38,64 +37,9 @@ const WhyClusttr = () => {
 							Connect with like-minded co-owners based on your preferences,
 							goals, and investment strategies.
 						</div>
-						<div className="platform_container">
-							<div className="first_dotted_box"></div>
-							<div className="platform_inner_container">
-								<div className="platform_top_container">
-									<div className="platform_top_first_stack">
-										<div>4 bedroom duplex</div>
-										{/* <Link to="/" >View details</Link> */}
-										<div>View details</div>
-									</div>
-									<div className="platform_top_second_stack">
-										<div></div>
-										<div></div>
-									</div>
-									<div className="platform_top_third_stack">
-										<div>
-											<div></div>
-											<div>$2,245.05</div>
-											<div></div>
-										</div>
-										<div>70% subscribed</div>
-									</div>
-								</div>
-								<div className="platform_bottom_container">
-									<div className="platform_bottom_users">
-										<div className="fake_user_container">
-											<img
-												src={person1}
-												alt="fake_users"
-												className="fake_users"
-											/>
-										</div>
-										<div className="fake_user_container">
-											<img
-												src={person2}
-												alt="fake_users"
-												className="fake_users"
-											/>
-										</div>
-										<div className="fake_user_container">
-											<img
-												src={person3}
-												alt="fake_users"
-												className="fake_users"
-											/>
-										</div>
-										<div className="fake_user_container">
-											<img
-												src={person4}
-												alt="fake_users"
-												className="fake_users"
-											/>
-										</div>
-									</div>
-									<div className='fake_user_amount_subscribed'>+12 others are subscribed</div>
-								</div>
-							</div>
-							<div className="second_dotted_box"></div>
-							<div className="background_grid"></div>
+						<WhyClusttrCoOwnershipDesign />
+						<div className="background_grid">
+							<img src={grid} alt="grid" style={{ width: '100%' }} />
 						</div>
 					</div>
 				</div>
@@ -179,13 +123,21 @@ const WhyClusttrStyle = styled.div`
 		gap: 15px;
 	}
 	& .why_clusttr_box {
-		background-color: #081517;
+		background-color: #050e11;
 		border-radius: 20px;
 		display: flex;
 		flex-direction: column;
 		gap: 11px;
 		border: 2px solid #132325;
-		padding: 60px 20px 125px;
+		padding: 120px 20px 65px;
+		position: relative;
+		overflow: hidden;
+	}
+	.background_grid {
+		bottom: 0;
+		right: 0;
+		width: 50%;
+		position: absolute;
 	}
 	// & .why_clusttr_big_box {
 	// 	padding: 60px 20px 125px;
@@ -213,90 +165,8 @@ const WhyClusttrStyle = styled.div`
 		font-weight: 500;
 		font-size: 0.9rem;
 		color: #5d7a76;
+		padding-bottom: 80px;
 		line-height: 22px;
-	}
-
-	// Intelligent Co-Ownership Platform
-	.platform_container {
-		width: 80%;
-		// position: relative;
-	}
-	.platform_container > .first_dotted_box,
-	.second_dotted_box {
-		border-radius: 10px;
-		padding: 40px 0;
-		width: 50%;
-		// position: absolute;
-	}
-	.first_dotted_box {
-		border: 2px dashed #00c187;
-		border-left: none;
-		border-bottom: none;
-	}
-	.second_dotted_box {
-		border: 2px dashed #00c187;
-		border-top: none;
-		border-right: none;
-	}
-	.platform_inner_container {
-		display: flex;
-		flex-direction: column;
-	}
-	.platform_top_container {
-		display: flex;
-		flex-direction: column;
-		background-color: #0b191b;
-	}
-	.platform_top_first_stack {
-		display: flex;
-		justify-content: space-between;
-	}
-	.platform_top_third_stack {
-		display: flex;
-		justify-content: space-between;
-	}
-	.platform_bottom_container {
-		display: flex;
-		padding: 40px 0;
-		
-	}
-	.platform_bottom_users {
-		width:20%;
-		// width: 100%;
-		display:flex;
-		align-items: center;
-		position: relative;
-	}
-	.platform_bottom_users > div:first-child {
-		background-color: #ffffff;
-	}
-	.platform_bottom_users > div:nth-child(2) {
-		left: 13px;
-		background-color: #9ce3f1;
-	}
-	.platform_bottom_users > div:nth-child(3) {
-		left: 26px;
-		display: flex;
-		background-color: #f7def4;
-	}
-	.platform_bottom_users > div:nth-child(4) {
-		left: 39px;
-		background-color: #9ce3f1;
-	}
-	.fake_user_container {
-		width: 20px;
-		height: 20px;
-		border-radius: 50%;
-		border: 1px solid #00c187;
-		overflow: hidden;
-		position: absolute;
-		top: 0;
-	}
-	.fake_users {
-		width: 125%;
-	}
-	.fake_user_amount_subscribed{
-		font-size: 0.7rem;
 	}
 
 	@media (max-width: 480px) {
