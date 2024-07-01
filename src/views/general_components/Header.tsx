177798a0fd3495ebbaaef948bc1e95ba5c6926dc
Matthew Chukwuemeka <martins.twist@gmail.com>
2024-07-01 13:44:38 +0100

    fix whitepapre url

--- src/views/general_components/Header.tsx
+++ src/views/general_components/Header.tsx
@@ -102,7 +102,7 @@ function Header({ open, setOpen }: { open: boolean; setOpen: any }) {
 							<a
 								id="whitepaper"
 								target="_blank"
-								href="https://clusttr.gitbook.io/clusttrs-white-paper/"
+								href="https://t.co/O5rj43pBFv"
 							>
 								Litepaper
 							</a>
